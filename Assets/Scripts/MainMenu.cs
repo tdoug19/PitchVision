c02e2a824143308174054a6ae94a13252273d2f2
Trevor Douglas <tdoug19@gmail.com>
2015-09-25 13:29:22 -0600

    Committing the project with the baseball diamond and  baseball models.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	
+    void OnGUI()
+    {
+        GUILayout.Button("Ball spin detection");
+    }
+
+
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
