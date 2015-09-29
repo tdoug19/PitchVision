c728e75213131b5464a30aa36ca0a6e11391ff1a
Trevor Douglas <tdoug19@gmail.com>
2015-09-29 09:57:34 -0600

    Created a Game Menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour {
+
+    public Canvas startMenu;
+    public Button spinDetectionText;
+    public Button exitText;
+    
+
+    //Use this for initialization
+    void Start () {
+
+        startMenu = startMenu.GetComponent<Canvas>();
+
+        spinDetectionText = spinDetectionText.GetComponent<Button>();
+
+
+
+
+    }
+	
+	public void spinDetectionPressed()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void exitGame()
+    {
+
+
+    }
+
+
+}
